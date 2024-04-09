92f97675922207f8e8da5cd40435a753661b7c7b
oohasikazuyuki <cdok1230245@gn.iwasaki.ac.jp>
2024-04-09 19:43:41 +0900

    reactの表示の切り替え修正

--- src/resources/js/components/App.jsx
+++ src/resources/js/components/App.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
-const app = () => {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    )
-    if(document.getElementById('app')){
-        ReactDOM.render(<App />, document.getElementById('app'));
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-header">Example Component</div>
+
+                            <div className="card-body">I'm an example component!</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
+
+
